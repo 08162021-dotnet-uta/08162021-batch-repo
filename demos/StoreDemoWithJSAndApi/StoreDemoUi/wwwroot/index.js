2cd698cbd4358b3d008a9a72f20a72d31f1719cf
Mark Moore <mark.moore@revature.com>
2021-09-13 16:58:23 -0500

    updates console Store app

--- demos/StoreDemoWithJSAndApi/StoreDemoUi/wwwroot/index.js
+++ demos/StoreDemoWithJSAndApi/StoreDemoUi/wwwroot/index.js
@@ -1,24 +1,43 @@
 const button = document.querySelector('.weatherlist');
 const listofcustomers = document.querySelector('.listofcustomers');
+const randombutton = document.querySelector(".randombutton");
 
 button.addEventListener('click', (e) => {
-
-  fetch('WeatherForecast')
-  .then(res => {
-    if (!res.ok) {
-      console.log('NOT OK')
-      throw new Error(`Network response was not ok (${res.status})`);
-    }
-    else {
-      return res.json();
-    }
-  })
-  .then(res => {
-    console.log(res);
-    //res.foreach(x => listofcustomers.innerHTML += x)
-    for (let x = 0; x < res.length;x++) {
-      listofcustomers.innerText += `The temp is ${res[x].temperatureC}.\nThe weather is ${res[x].summary}.\n`
-    }
-  })
-    .catch(err => console.log(`There was an error ${err}`));
+	fetch('WeatherForecast')
+		.then(res => {
+			if (!res.ok) {
+				console.log('NOT OK')
+				throw new Error(`Network response was not ok (${res.status})`);
+			}
+			else {
+				return res.json();
+			}
+		})
+		.then(res => {
+			console.log(res);
+			//res.foreach(x => listofcustomers.innerHTML += x)
+			for (let x = 0; x < res.length; x++) {
+				listofcustomers.innerText += `The temp is ${res[x].temperatureC}.\nThe weather is ${res[x].summary}.\n`
+			}
+		})
+		.catch(err => console.log(`There was an error ${err}`));
 });
+
+randombutton.addEventListener('click', (e) => {
+	fetch('WeatherForecast/thisisapost')
+		.then(res => {
+			if (!res.ok) {
+				console.log('NOT OK')
+				throw new Error(`Network response was not ok (${res.status})`);
+			}
+			else {
+				console.log(res);
+				return res.text();
+			}
+		})
+		.then(res => {
+			console.log(res);
+		})
+		.catch(err => console.log(`There was an error ${err}`));
+});
+
